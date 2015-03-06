2e3b6b978118e7bbaec584932b9b08d36faec253
Joseph LaFreniere <lafrenierejm@gmail.com>
2015-03-06 17:01:45 -0600

    Changed from array implementation to doubly linked list. Prevents having to discretely calculate length of bitstring.

--- LaTeX_Generator.c
+++ LaTeX_Generator.c
@@ -1,115 +1,187 @@
+/* Performs exponential modulation of unsigned integers
+ * Each step is written to the local file Output.tex
+ *
+ * The binary conversion of the power is stored as a linked list
+ */
+
+// Preprocessor directives
 #include <stdio.h>
 #include <stdlib.h>
 
-main() {
-	/*	Function Prototypes	*/
-	short DecimalBitLength(unsigned decimal);
-	void DecimalToBitstring(unsigned decimal, short bitstring[], short numBits, FILE *outputFile);
-	unsigned PerformModulation(unsigned b, short bitstring[], short numElements, unsigned modulus, FILE *outputFile);
-	
-	/*	Variables	*/
+// Define node for linked list
+struct node {
+	char bit;		// Holds node's value
+	struct node *prev;	// Points to prev node
+	struct node *next;	// Points to next node
+};
+
+// Function prototypes
+struct node* MakeNode(char value, struct node *before);	// Create new node
+void DeleteList(struct node *head);	// Delete linked list
+struct node* DecimalToBitstring(unsigned decimal, struct node *head, FILE *outputFile);	// Convert 'decimal' to bitstring
+short PerformModulation(unsigned b, struct node *tail, unsigned m, FILE *outputFile);	// Ouput exponential modulation
+
+// Main method
+main() {		
 	unsigned b, n, m;	// b^n mod m
-		printf("Enter b, n, and m: ");
-		scanf("%u %u %u", &b, &n, &m);
-	short numBits = DecimalBitLength(n);	// Holds length of bistring of b
-	unsigned result;	// Final result of exponential modulation
-	short binary[numBits];	// Array of ints to hold binary representation
-	// File	variables
+	short result;	// Final result of exponential modulation
+	struct node *head;	// Pointer directed at first node in list
+	struct node *tail;	// Pointer directed at last node in list
 	char outputFilename[] = "Output.tex";
 	FILE *outputFile = fopen(outputFilename, "w");
 	
-	/*	Initial file output	*/
-	fprintf(outputFile, "$b$ = %d, $n$ = %d, $m$ = %d\n", b, n, m);
+	head = MakeNode('z', NULL);	// Create first node with prev = NULL
 
-	/*	Solve	*/
-	DecimalToBitstring(n, binary, numBits, outputFile);	// Store binary representation of b in array binary
-	// outputFile contains text converting to binary
+	// Get user input
+	printf("Enter b, n, and m: ");
+	scanf("%u %u %u", &b, &n, &m);
+
+	// Initial file output
+	fprintf(outputFile, "\\(b\\) = %d, \\(n\\) = %d, \\(m\\) = %d\n", b, n, m);
+
+	// Store n's binary representation in list
+	tail = DecimalToBitstring(n, head, outputFile);
+
+	// outputFile now contains steps of binary conversion
 	fprintf(outputFile, "\\\\\n\n");
-	result = PerformModulation(b, binary, numBits, m, outputFile);
-	fprintf(outputFile, "\n$%d^{%d} \\bmod %d = \\ans{%d}$",
-		b, n, m, result);	// output result
+
+	result = PerformModulation(b, tail, m, outputFile);
+	fprintf(outputFile, "\n\\(%d^{%d} \\bmod %d = \\ans{%d}", // Write final result
+		b, n, m, result);
 	
+	// Free memory used by list
+	DeleteList(head);
+
+	// Exit cleanly
 	exit(EXIT_SUCCESS);
 }
 
-/*	Calculate the number of bits requred to represent int decimal	*/
-short DecimalBitLength(unsigned decimal) {
-	short numberPlaces = 0;
-	while(decimal > 0) {
-		numberPlaces++;
-		decimal /= 2;
+// Method to create new node
+struct node* MakeNode(char value, struct node *before) {
+	struct node *newNode;	// Create new node pointer
+	newNode = (struct node*)malloc(sizeof(struct node));	// Allocate memory for new node
+
+	if(newNode == NULL) {	// Insufficient memory
+		printf("Node creation failed.\n");
+		return NULL;	// Return with NULL pointer
 	}
 
-	return numberPlaces;
+	newNode->bit = value;		// Store num as node's value
+	newNode->prev = before;		// Point back in list
+	newNode->next = NULL;		// Next element is NULL
+	
+	return newNode;
 }
 
-/*	Fill bitstring array of length numBits with the binary representaition of decimal	*/
-void DecimalToBitstring(unsigned decimal, short bitstring[], const short numBits, FILE *outputFile) {		
-	short tmpNumBits = numBits;
-	fprintf(outputFile, "\\begin{align*}\n");
+// Delete all nodes in list
+void DeleteList(struct node *head) {
+	struct node *ptr;	// New node pointer
+	ptr = head->next;	// Point to first node
 	
-	while(tmpNumBits > 0) {	
-		bitstring[tmpNumBits] = decimal % 2;
-		decimal /= 2;	// Divide decimal
+	// Free memory up to last node
+	while(ptr->next) {
+		free(head);	// Delete node
+		head = ptr;	// Advance head
+		ptr = ptr->next;// Advance ptr
+	}
 
-		/*	Output tex markup	*/
-		fprintf(outputFile, "\t%d / 2 &= %d\t&& %d",
-			decimal * 2, decimal, bitstring[tmpNumBits]);
-		if(tmpNumBits != 1)
+	// Free final node
+	free(ptr);
+}
+
+// Storebitstring representation of decimal in linked list
+// Returns length of that binary representation
+struct node* DecimalToBitstring(unsigned decimal, struct node *head, FILE *outputFile) {		
+	struct node *ptr = head;// Point new pointer at head
+	struct node *msb;	// Pointer for most significant bit
+	ptr = head;		// Point ptr at head of list
+
+	fprintf(outputFile, "\\begin{align*}\n");	// Begin TeX align environment
+	
+	// Loop while decimal is greater than 0
+	while(decimal != 0) {
+		ptr->bit = '0' + (decimal % 2);	// Store remainder as char in list
+		decimal /= 2;			// Divide decimal
+
+		// Output tex markup
+		fprintf(outputFile, "\t%d / 2 &= %d\t&& %c",
+			decimal * 2, decimal, ptr->bit);
+		if(decimal >= 1)	// More than one iteration left
 			fprintf(outputFile, "\\\\");
 		fprintf(outputFile, "\n");
-
-		tmpNumBits--;	// Decrement array index
+		
+//		printf("%c\n", ptr->bit);	// Display bit
+		ptr->next = MakeNode('z', ptr);	// Create new end of list
+		ptr = ptr->next;		// Point to end of list
 	}
 
-	/*	Output complete bitstring	*/
-	tmpNumBits = 1;
+	// Go back one step from end of list, returning to MSB
+	ptr = ptr->prev;
+	msb = ptr;	// Assign msb pointer
+
+	// Output complete bitstring
 	fprintf(outputFile, "\\end{align*}\n");
-	fprintf(outputFile, "So binary representation of $n$ is $");
-	while(tmpNumBits <= numBits) {
-		fprintf(outputFile, "%d", bitstring[tmpNumBits]);
-		tmpNumBits++;
+	fprintf(outputFile, "So binary representation of \\(n\\) is \\(");
+
+	while(ptr->prev) {		// Loop backwards until beginning of list
+		fprintf(outputFile, "%c", ptr->bit);	// Print out bit
+		ptr = ptr->prev;			// Go to next node
 	}
-	fprintf(outputFile, "_2$");
+	fprintf(outputFile, "_2\\)");
 	
-	return;	// Exit returning void
+	return msb;	// Exit returning pointer to MSB
 }
 
-unsigned PerformModulation(unsigned b, short a[], short numElements, unsigned m, FILE *outputFile) {
-	short x = 1;
-	unsigned power = b % m;
-	short tmpNumElements = numElements;
-	
-	fprintf(outputFile, "$x = %d$\t\\\\\n$\\text{power} = %d$",
+// Write process of calculating x and power to output file
+// Return final remainder
+short PerformModulation(unsigned b, struct node *tail, unsigned m, FILE *outputFile) {
+	struct node *ptr;	// Create new pointer, point at start of list
+	short x = 1;		// Instantiate x with value 1
+	unsigned power = b % m;	// Power = b mod m
+	int currentPosition;	// Used for subscript of a in TeX output
+
+
+	// Write initial values of x, power
+	fprintf(outputFile, "\\(x = %d\\(\t\\\\\n\\)\\text{power} = %d\\)",
 		x, power);
 	fprintf(outputFile, "\n\n");
+
+	// Loop through list backwards
+	ptr = tail;		// Now pointing at MSB
+	currentPosition = 0;	// Begin at a_0
+
+	while(ptr != NULL) {
+
+		// Debug
+		printf("%c\n", ptr->bit);
+
+		// Output value of a_currentPosition
+		fprintf(outputFile, "\\(a_{%d} = %c\\), ",
+			currentPosition, ptr->bit);
 		
-	while(numElements > 0) {
-		fprintf(outputFile, "$a_{%d} = %d$, ",
-			tmpNumElements - numElements, a[numElements]);
-		
-		// calculate x
-		if(a[numElements] == 1) {
-			fprintf(outputFile, "$x = (%d * %d) \\bmod %d = ",
+		// Calculate x
+		if(ptr->bit == '1') {	
+			fprintf(outputFile, "\\(x = (%d * %d) \\bmod %d = ",
 				x, power, m);
 			x = (x * power) % m;
-			fprintf(outputFile, "%d$; ", x);
+			fprintf(outputFile, "%d\\); ", x);
 		}
 		else
-			fprintf(outputFile, "$x = %d$; ", x);
+			fprintf(outputFile, "\\(x = %d\\); ", x);
 
-		// calculate power
-		if(numElements != 1) {
-			fprintf(outputFile, "power = $%d^2 \\bmod %d = ", power, m);
+		// Calculate power
+		if(ptr->prev != NULL) {
+			fprintf(outputFile, "power = \\(%d^2 \\bmod %d = ", power, m);
 			power = (power * power) % m;
-			fprintf(outputFile, "%d$\\\\\n", power);
+			fprintf(outputFile, "%d\\)\\\\\n", power);
 		}
 		else
 			fprintf(outputFile, "\\\\\n");
-	
-		// Decrement numElements
-		numElements--;
+		
+		currentPosition++;	// Increment currentPosition
+		ptr = ptr->prev;	// Advance to prev node
 	}
 
+	x = 1;
 	return x;	// Return final remainder 
 }
